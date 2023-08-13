72880bef85b562ecd0cba657eadb97c22e6dc6aa
nanana666 <battleshipbb63@163.com>
2023-08-14 00:24:21 +0800

    recordForm页面

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import Particles from "particles.vue3";
+import Particles from "particles.vue3"
+import VueQuillEditor from 'vue-quill-editor' // import rich text editor
+import "quill/dist/quill.core.css" 
+import "quill/dist/quill.snow.css" 
+import "quill/dist/quill.bubble.css"; 
 
+Vue.use(VueQuillEditor)
 createApp(App)
 .use(Particles)
 .use(ElementPlus)
