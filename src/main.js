37567367ec41ca4896a9ecb1e0cd572a2582547a
euyis1019 <122277342+euyis1019@users.noreply.github.com>
2023-08-22 13:53:01 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import Particles from "particles.vue3";
-
+//按照VUE3的方式去注册！
+//本地能运行再commit
 createApp(App)
 .use(Particles)
 .use(ElementPlus)
