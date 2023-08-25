1d276a0f098e588307be1ad0290aed304c13fbea
weixian <3342764301@qq.com>
2023-08-25 14:29:08 +0800

    美化mainbox和report

--- src/views/MainBox.vue
+++ src/views/MainBox.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        <el-button type="info" @click="handleClick">申请记录</el-button>
-    </div>
-    <div>
-        <el-button type="info" @click="handleClick1" round>提交申请</el-button>
-    </div>
+    <el-row class="tac">
+        <el-col :span="3">
+            <h1>导航条</h1>
+            <el-menu>
+                <el-menu-item index="1" @click="handleClick">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">申请记录</span>
+                </el-menu-item>
+                <el-menu-item index="2" @click="handleClick1">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">提交申请</span>
+                </el-menu-item>
+            </el-menu>
+        </el-col>
+    </el-row>
+
     <RouterView></RouterView>
 </template>
+
+
+
 
 
 <script>
@@ -17,6 +30,12 @@ export default {
         },
         handleClick1() {
             this.$router.push('/Report')
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
         },
     }
 }
